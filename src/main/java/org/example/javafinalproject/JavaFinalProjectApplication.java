c99445d4feb6c789dc95a8bce3f533a0f8a005e8
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-06-16 03:17:53 +0700

    initial setup

---
+++ src/main/java/org/example/javafinalproject/JavaFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.javafinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/javafinalproject/JavaFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javafinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
