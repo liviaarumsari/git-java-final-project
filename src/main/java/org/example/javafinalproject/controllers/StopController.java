04d3602ba69ade1c4f46c4644a34276f5982c2f0
syahba <ksyaahba@gmail.com>
2024-07-10 23:52:35 +0700

    create stop order api

--- src/main/java/org/example/javafinalproject/controllers/StopController.java
+++ src/main/java/org/example/javafinalproject/controllers/StopController.java
@@ -5,12 +5,19 @@ import org.example.javafinalproject.payloads.response.ApiResponseBuilder;
 import org.example.javafinalproject.repository.StopRepository;
 import org.example.javafinalproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.persistence.criteria.CriteriaBuilder.In;
+
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -30,6 +37,42 @@ public class StopController {
 
         List<Stop> stops = stopRepository.findAll();
 
+        return ResponseEntity.ok().body(ApiResponseBuilder.buildSuccessResponse(stops));
+    }
+
+    @GetMapping("/order")
+    public ResponseEntity<?> getStopOrder(
+        @RequestParam(required = true) Long sourceStop,
+        @RequestParam(required = true) Long destStop
+    ) {
+        Optional<Stop> sourceNumber = stopRepository.findById(sourceStop);
+        Optional<Stop> destNumber = stopRepository.findById(destStop);
+
+        Integer stopOne = sourceNumber.get().getNumber();
+        Integer stopTwo = destNumber.get().getNumber();
+
+        Integer bigger;
+        Integer smaller;
+        if (stopOne > stopTwo) {
+            bigger = stopOne;
+            smaller = stopTwo;
+        } else {
+            bigger = stopTwo;
+            smaller = stopOne;
+        }
+
+        List<Integer> numbers = new ArrayList<Integer>();
+        for (Integer i = smaller; numbers.size() < bigger; i++) {
+            numbers.add(i);
+        }
+
+        List<Stop> stops;
+        if (stopTwo == bigger) {
+            stops = stopRepository.findByNumberIn(numbers, Sort.by(Sort.Direction.ASC, "number"));
+        } else {
+            stops = stopRepository.findByNumberIn(numbers, Sort.by(Sort.Direction.DESC, "number"));
+        }
+        
         return ResponseEntity.ok().body(ApiResponseBuilder.buildSuccessResponse(stops));
     }
 }
