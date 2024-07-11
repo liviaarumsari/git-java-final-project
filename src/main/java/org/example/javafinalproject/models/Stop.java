f1d06f2b9e2a6029720606014c9988d62b256897
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-07-11 22:34:48 +0700

    fix: change int validation to not null

--- src/main/java/org/example/javafinalproject/models/Stop.java
+++ src/main/java/org/example/javafinalproject/models/Stop.java
@@ -3,6 +3,7 @@ package org.example.javafinalproject.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,7 +28,7 @@ public class Stop {
     @Size(max = 255)
     private String detail;
 
-    @NotBlank
+    @NotNull
     @Min(1)
     private Integer number;
 
